6a210b1c951cf21e1db818d98ba2ad8f5b0fa8fc
TranThoiLong <tranthoilong.giaitri@gmail.com>
2022-11-10 08:10:12 +0700

    contitnute1

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main(List<String> args) {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  var count = 0.obs;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Counter APP"),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          count++;
+        }),
+        body: Center(
+          child: Obx(
+            () => Text(
+              "$count",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 50,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
